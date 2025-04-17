fc42a21b2ed823723d07db9187b931a6d834feb9
cmont13 <cmontie0922@gmail.com>
2025-04-17 09:23:52 -0400

    Updated APP.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with ui.layout_columns():
         @render.text
         def text0():
             try:
-                df = pbp.loc[int(input.batter()), str(input.if_alignment()), str(input.of_alignment())]
+                df = pbp.loc[input.batter(), str(input.if_alignment()), str(input.of_alignment())]
                 return str(round(df['wOBA'].mean(), 3))
             except:
                 return
@@ -22,10 +22,10 @@ with ui.layout_columns():
         @render.text
         def text1():
             try:
-                st =  pbp.loc[int(input.batter()), 'st', 'st']
-                df = pbp.loc[int(input.batter()), str(input.if_alignment()), str(input.of_alignment())]
+                st =  pbp.loc[input.batter(), 'st', 'st']
+                df = pbp.loc[input.batter(), str(input.if_alignment()), str(input.of_alignment())]
                 st_woba = st['wOBA'].mean()
-                return str(round(st_woba - df['wOBA'].mean(), 3))
+                return str(round(df['wOBA'].mean() - st_woba, 3))
             except:
                 return
     with ui.card():
@@ -33,14 +33,14 @@ with ui.layout_columns():
         @render.text
         def text2():
             try:
-                df = pbp.loc[int(input.batter()), str(input.if_alignment()), str(input.of_alignment())]
+                df = pbp.loc[input.batter(), str(input.if_alignment()), str(input.of_alignment())]
                 return str(len(df))
             except:
                 return
 
 
 with ui.card():
-    ui.card_header('Heatmap', class_='bg-primary lead')
+    ui.card_header('Spray Chart', class_='bg-primary lead')
     @render.plot
     def plot():
         import matplotlib.pyplot as plt
@@ -54,8 +54,17 @@ with ui.card():
         ax.grid(visible=False)
         ax.set_xticklabels([])
 
+        if_theta = [0.785398, 0, -0.785398, 0, 0.785398]
+        if_r = [90, 120, 90, 0, 90]
+
+        ax.fill_between(np.linspace(-0.785398, 0.785398, 100), 0, 450, color='#065700', alpha=1, zorder=0, label='_nolegend_')
+        ax.fill_between(np.linspace(-0.785398, 0.785398, 100), 0, 160, color='#f5e09d', alpha=1, zorder=1, label='_nolegend_')
+        ax.fill_between(np.linspace(-0.785398, 0.785398, 100), 0, 100, color='#065700', alpha=1, zorder=2, label='_nolegend_')
+        ax.scatter([0], [60.5], c='#f5e09d', zorder=3, label='_nolegend_')
+        ax.plot(if_theta, if_r, c='#f5e09d', linewidth=10, zorder=3, label='_nolegend_')
+
         try:
-            df = pbp.loc[int(input.batter()), str(input.if_alignment()), str(input.of_alignment())]
+            df = pbp.loc[input.batter(), str(input.if_alignment()), str(input.of_alignment())]
 
             pos_r = (list(df.loc[:, 'fielder_3_r']) +
                 list(df.loc[:, 'fielder_4_r']) +
@@ -74,19 +83,24 @@ with ui.card():
                 list(df.loc[:, 'fielder_9_a']))
             
             hit_x = df['hc_x'] - 115
-            hit_y = df['hc_y'] - 215
+            hit_y = df['hc_y'] - 200
 
             hit_a = np.arctan(hit_x / hit_y)
             hit_r = df['hit_distance_sc']
 
-            ax.scatter(hit_a, hit_r, s=4)
-            ax.scatter(pos_a, pos_r)
+            ax.scatter(pos_a, pos_r, c='lightblue', alpha=.1, zorder=4)
+            ax.scatter(hit_a, hit_r, c='#ff4726', s=4, zorder=5)
+
+            ax.scatter(hit_a[hit_r < 160].mean(), hit_r[hit_r < 160].mean(), c='#44ff33', s=75, zorder=5)
+            ax.scatter(hit_a[hit_r >= 160].mean(), hit_r[hit_r >= 160].mean(), c='#44ff33', s=75, zorder=5)
+
+            ax.legend(['Fielders', 'Balls In Play', 'Average Hit\nLocation'], loc='lower left')
         except:
             pass
         return fig
 
 with ui.sidebar(bg='#3a9098', fg='#ffffff'):
-    ui.input_select(
+    ui.input_selectize(
         'batter', 'Select Batter',
         choices=batters
     ),
--- shared.py
+++ shared.py
@@ -2,5 +2,7 @@ from pathlib import Path
 import pandas as pd
 
 app_dir = Path(__file__).parent
-pbp = pd.read_parquet(app_dir / 'data/play_by_play.parquet').set_index(['batter', 'if_fielding_alignment', 'of_fielding_alignment'])
-batters = dict(zip([str(i[0]) for i in set(pbp.index)], list(pbp['batter_name_y'])))
+pbp = pd.read_parquet(app_dir / 'data/play_by_play.parquet').set_index(['batter_name_y', 'if_fielding_alignment', 'of_fielding_alignment']).sort_index()
+batters = list(i[0] for i in list(set(pbp.index)))
+batters.sort()
+
